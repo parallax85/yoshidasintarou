357f3772b4dfa4531cfe649ee9ebe3bfe2d73ea4
parallax85 <136150390+parallax85@users.noreply.github.com>
2024-03-09 15:10:57 +0900

    Delete syakyou3 directory

--- syakyou3/main.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    // ハンバーガーボタンクリックで実行
-    $(".drawer_button").click(function () {
-      $(this).toggleClass("active");
-      $(".drawer_nav").toggleClass("active");
-    });
-    // function
-  });
